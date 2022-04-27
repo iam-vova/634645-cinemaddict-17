d532b752bdeeb0c00ce245bdaabe7d21f269dffe
brylev.v <iam-vova@yandex.ru>
2022-04-27 20:17:15 +0300

    M1T3 | Добавляет модули и презентер

---
+++ src/view/films-container-view.js
@@ -0,0 +1,36 @@
+import {createElement} from '../render.js';
+
+const createFilmsContainerTemplate = () => (
+  `<section class="films">
+    <section class="films-list">
+      <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+      <div class="films-list__container"></div>
+    </section>
+    <section class="films-list films-list--extra">
+      <h2 class="films-list__title">Top rated</h2>
+      <div class="films-list__container"></div>
+    </section>
+    <section class="films-list films-list--extra">
+      <h2 class="films-list__title">Most commented</h2>
+      <div class="films-list__container"></div>
+    </section>
+  </section>`
+);
+
+export default class FilmsContainerView {
+  getTemplate() {
+    return createFilmsContainerTemplate();
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
