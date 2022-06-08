9d2f58f4878b39f94e47d3320fe1ef3f02dab88a
brylev.v <iam-vova@yandex.ru>
2022-06-08 23:11:51 +0300

    M7T1 | Реализует безопасный ввод

--- src/view/comment-add-view.js
+++ src/view/comment-add-view.js
@@ -1,5 +1,6 @@
 import AbstractStatefulView from '../framework/view/abstract-stateful-view';
 import {emojiNames} from '../constants';
+import he from 'he';
 
 const createCommentAddTemplate = (comment) => {
   const {emotion, text} = comment;
@@ -14,7 +15,7 @@ const createCommentAddTemplate = (comment) => {
           class="film-details__comment-input"
           placeholder="Select reaction below and write comment here"
           name="comment"
-          >${text}</textarea>
+          >${he.encode(text)}</textarea>
       </label>
       <div class="film-details__emoji-list">
         ${emojiNames.map((item) => (
--- src/view/comment-view.js
+++ src/view/comment-view.js
@@ -1,4 +1,5 @@
 import AbstractView from '../framework/view/abstract-view';
+import he from 'he';
 import {humanizeCommentDate} from '../utils/film';
 
 const createCommentTemplate = (item) => {
@@ -10,7 +11,7 @@ const createCommentTemplate = (item) => {
         <img src="./images/emoji/${emotion}.png" width="55" height="55" alt="emoji-${emotion}">
       </span>
       <div>
-        <p class="film-details__comment-text">${text}</p>
+        <p class="film-details__comment-text">${he.encode(text)}</p>
         <p class="film-details__comment-info">
           <span class="film-details__comment-author">${author}</span>
           <span class="film-details__comment-day">${humanizeCommentDate(date)}</span>
